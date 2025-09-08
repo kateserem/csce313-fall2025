9930f137061ae6906d6c8d9cc7c3b8be3d3edbd9
Kate Serem <kserem2004@tamu.edu>
2025-09-08 02:21:49 +0000

    Implement pipe between ls and tr in lab2

--- lab2/LE-2 Starter Code/shell.cpp
+++ lab2/LE-2 Starter Code/shell.cpp
@@ -5,7 +5,7 @@ LE2: Introduction to Unnamed Pipes
 #include <iostream> 
 
 
-#include <sys/wait.h>
+#include <sys/wait.h> //for wait
 
 using namespace std;
 
@@ -27,20 +27,29 @@ int main () {
     }
 
     // Create child to run first command
-    pid_t pid = fork();
+    pid_t pid1 = fork();
 
-    if (pid == -1) {
+    if (pid1 == -1) {
         cerr << "fork failed\n";
         return 1;
     }
 
 
-    if (pid == 0) {
-        // In child, redirect output to write end of pipe
+    if (pid1 == 0) {
         cout << "Hello from the CHILD process! \n";
 
-        // close(p[0]); //close the read end 
+        // In child, redirect output to write end of pipe
+        dup2(p[1], STDOUT_FILENO); 
         
+        // Close the read end of the pipe on the child side.
+        close(p[0]); 
+
+        // In child, execute the command
+        execvp(cmd1[0], cmd1); 
+        perror("execvp"); //if execvp fails
+
+        close(p[1]); //closing the write end after duplicating it to stdout
+
         return 0;
     } else {
         cout << "Hello from the PARENT process! \n";
@@ -48,13 +57,33 @@ int main () {
     }
 
 
-    // Close the read end of the pipe on the child side.
-    // In child, execute the command
-
     // Create another child to run second command
-    // In child, redirect input to the read end of the pipe
-    // Close the write end of the pipe on the child side.
-    // Execute the second command.
+    pid_t pid2 = fork();
+
+        if (pid2 == -1) {
+        cerr << "fork failed\n";
+        return 1;
+    }
+
+    if (pid2 == 0) {
+        // In child, redirect input to the read end of the pipe
+        dup2(p[0], STDIN_FILENO); 
+        // Close the write end of the pipe on the child side.
+        close(p[1]); 
+
+        // In child, execute the command
+        execvp(cmd2[0], cmd2); 
+        perror("execvp"); //if execvp fails
+
+        close(p[0]); //closing the read end after duplicating it to stdin
+
+        return 0;
+    } else {
+        wait(nullptr); //making parent pause until the child finishes 
+    }
+
+    close(p[0]);
+    close(p[1]);
 
     // Reset the input and output file descriptors of the parent.
 }
